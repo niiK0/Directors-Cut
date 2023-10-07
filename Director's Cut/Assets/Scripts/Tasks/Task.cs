64fd0634d268afeed1ad676149cec3b778cc991d
Celestino <45713831+Cell21@users.noreply.github.com>
2023-10-07 05:06:23 +0100

    Mega update tasks
    4h
    Preciso de falar com o nico e o Tiago

--- Director's Cut/Assets/Scripts/Tasks/Task.cs
+++ Director's Cut/Assets/Scripts/Tasks/Task.cs
@@ -8,20 +8,21 @@ public class Task
     public bool isDoing;
     public bool isComplete;
     public bool isDirector;
-    public int[] taskSteps;
     //public Player source;
     public float taskRange;
     public float completePercentage;
+    public Steps[] taskSteps;
 
-    public Task(string Name, bool Doing, bool Complete, bool Director, int[] steps, float Range, float Percentage)
+    public Task(string Name, bool Doing, bool Complete, bool Director, float Range, float Percentage, Steps[] steps)
     {
 
         taskName= Name;
         isDoing= Doing;
         isComplete= Complete;
         isDirector= Director;
-        taskSteps = steps;
+        
         taskRange= Range;
         completePercentage= Percentage;
+        taskSteps = steps;
     }
 }
--- Director's Cut/Assets/Scripts/Tasks/TaskManager.cs
+++ Director's Cut/Assets/Scripts/Tasks/TaskManager.cs
@@ -8,16 +8,15 @@ public class TaskManager
     List<Steps> steps = new List<Steps>()
     {
         new Steps(false, "Andar", 0, false, false),
-        new Steps(true, "Wait", 0.01f, true, false),
-        new Steps(false, "Pick up bread", 1, true, false),
-        new Steps(false, "Pick up ham", 1, true, false),
+        new Steps(true, "Wait", 0.01f, false, false),
+        new Steps(false, "Pick up bread", 1, false, false),
+        new Steps(false, "Pick up ham", 1, false, false),
     };
  
     //Criar uma lista de tasks
     List<Task> tasks = new List<Task>() {
-        new Task("Cozinhar", false, false, false, new int[1] {1}, 10f, 0),
-        new Task("Beber", false, false, false, new int[1] {1}, 0, 0),
-        new Task("FAZER UMA ASSANDES", false, false, false, new int[2] {2,3}, 0, 0)
+        new Task("Cozinhar", false, false, false,0f,0f, new Steps[1] {new Steps(true, "Wait", 0.01f, true, false)}),
+        new Task("Beber", false, false, false, 0f, 0f, new Steps[2] {new Steps(true, "Wait", 0.01f, false, false), new Steps(true, "Wait", 0.01f, false, false)}),
     };
 
     //Criar funçao que receba o id da task e devolva a informação dela
@@ -26,9 +25,5 @@ public class TaskManager
         return (tasks[id]);
     }
 
-    //Função que vai buscar o step em questao
-    public Steps GetStepsById(int id)
-    {
-        return steps[id];
-    }
+    
 }
