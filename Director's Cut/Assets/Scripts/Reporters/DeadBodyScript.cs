da7a63a2a692e9e617a56cdd8d0dbe522788e271
niiK0 <37949374+niiK0@users.noreply.github.com>
2023-11-13 14:39:10 +0000

    Adjusted DeadBody script

--- Director's Cut/Assets/Scripts/Reporters/DeadBodyScript.cs
+++ Director's Cut/Assets/Scripts/Reporters/DeadBodyScript.cs
@@ -1,12 +1,50 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeadBodyScript : BaseReporter
 {
+    VoteManager voteManager;
+    [SerializeField] List<Transform> meetingPoints = new List<Transform>();
+
+    private void Start()
+    {
+        voteManager = VoteManager.Instance;
+    }
+
     protected override void ReporterFunction(GameObject playerObj)
     {
         Destroy(gameObject, 3f);
-        //Destroy(gameObject);
+        ResetMeetingPoints();
+        SeatPlayer(playerObj);
+        voteManager.StartMeeting();
+    }
+
+    private void ResetMeetingPoints()
+    {
+        meetingPoints.Clear();
+
+        foreach (Transform point in meetingPlace)
+        {
+            meetingPoints.Add(point);
+        }
+    }
+
+    public void SeatPlayer(GameObject player)
+    {
+        if (meetingPoints.Count == 0)
+        {
+            Debug.LogError("No available meeting points left!");
+            return;
+        }
+
+        int meetingPointIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+
+        player.transform.position = meetingPoints[meetingPointIndex].position;
+        player.transform.rotation = meetingPoints[meetingPointIndex].rotation;
+        //freeze player
+        player.GetComponent<PlayerController>().freezePlayer = true;
+        // add sitting animation
     }
 }
